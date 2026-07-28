05763c947c90073893c4d111504168daf344a8cd
ztyibvrtoh-blip <ztyibvrtoh@gmail.com>
2026-07-28 22:50:47 +0100

    Update server.js

--- server.js
+++ server.js
@@ -1,11 +1,13 @@
 const express = require("express");
 
 const app = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
-app.get("/", (req, res) => {
-  res.send("DRAGONNOIR Backend is running!");
-});
+// استيراد المسارات
+const routes = require("./routes");
+
+// استخدام المسارات
+app.use("/", routes);
 
 app.listen(PORT, () => {
   console.log(`DRAGONNOIR Backend started on port ${PORT}`);
