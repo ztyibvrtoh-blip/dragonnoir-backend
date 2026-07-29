675b94a8d0acae75125a68934ca84da6c297ba44
ztyibvrtoh-blip <ztyibvrtoh@gmail.com>
2026-07-29 09:54:48 +0100

    Update serverController.js

--- controllers/serverController.js
+++ controllers/serverController.js
@@ -7,6 +7,19 @@ const getApiInfo = (req, res) => {
   });
 };
 
+const getServers = (req, res) => {
+  res.json([
+    {
+      id: 1,
+      name: "My Survival Server",
+      version: "1.21.1",
+      status: "offline",
+      players: 0
+    }
+  ]);
+};
+
 module.exports = {
   getApiInfo,
+  getServers,
 };
