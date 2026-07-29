e0e1b5719b8452102a28a6bfbe4183803ffb8e0a
ztyibvrtoh-blip <ztyibvrtoh@gmail.com>
2026-07-29 09:34:05 +0100

    Create serverController.js

---
+++ controllers/serverController.js
@@ -0,0 +1,12 @@
+const getApiInfo = (req, res) => {
+  res.json({
+    name: "DRAGONNOIR API",
+    version: "1.0.0",
+    status: "online",
+    message: "Welcome to DRAGONNOIR Backend!"
+  });
+};
+
+module.exports = {
+  getApiInfo,
+};
