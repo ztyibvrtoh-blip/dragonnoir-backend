e593fadf0f097bc395819fc22a7845337dcceed5
ztyibvrtoh-blip <ztyibvrtoh@gmail.com>
2026-07-30 17:22:13 +0100

    Update paperDownloader.js

--- controllers/paperDownloader.js
+++ controllers/paperDownloader.js
@@ -1,1 +1,23 @@
+const axios = require("axios");
 
+async function getLatestPaperBuild(version = "1.21.1") {
+  try {
+    const response = await axios.get(
+      `https://fill.papermc.io/v3/projects/paper/versions/${version}/builds`,
+      {
+        headers: {
+          "User-Agent": "DRAGONNOIR/1.0"
+        }
+      }
+    );
+
+    return response.data;
+  } catch (err) {
+    console.error("Paper Downloader Error:", err.message);
+    return null;
+  }
+}
+
+module.exports = {
+  getLatestPaperBuild
+};
