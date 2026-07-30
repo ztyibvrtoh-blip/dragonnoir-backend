082408867f71d344bac4897bc0013f992b9009a8
ztyibvrtoh-blip <ztyibvrtoh@gmail.com>
2026-07-30 09:52:51 +0100

    Update index.js

--- routes/index.js
+++ routes/index.js
@@ -1,4 +1,12 @@
-controllers/serverController");
+const express = require("express");
+const router = express.Router();
+
+const {
+  getApiInfo,
+  getServers,
+  createServer,
+  getServerFolders,
+} = require("../controllers/serverController");
 
 // الصفحة الرئيسية
 router.get("/", getApiInfo);
@@ -6,7 +14,10 @@ router.get("/", getApiInfo);
 // عرض جميع السيرفرات
 router.get("/servers", getServers);
 
+// عرض مجلدات السيرفرات
+router.get("/servers/folders", getServerFolders);
+
 // إنشاء سيرفر جديد
 router.post("/servers", createServer);
 
-module.exports =
+module.exports = router;
