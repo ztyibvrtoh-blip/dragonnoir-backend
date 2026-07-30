3a18ad9e2346979c71791936474ee3470cbbfcc4
ztyibvrtoh-blip <ztyibvrtoh@gmail.com>
2026-07-30 17:31:41 +0100

    Update paperService.js

--- services/paperService.js
+++ services/paperService.js
@@ -1,1 +1,23 @@
+const axios = require("axios");
 
+async function getPaperVersions() {
+  try {
+    const response = await axios.get(
+      "https://fill.papermc.io/v3/projects/paper",
+      {
+        headers: {
+          "User-Agent": "DRAGONNOIR/1.0"
+        }
+      }
+    );
+
+    return response.data;
+  } catch (err) {
+    console.log("Paper Error:", err.response?.status || err.message);
+    return null;
+  }
+}
+
+module.exports = {
+  getPaperVersions
+};
