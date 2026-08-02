d57c59abfea8041d88da3e679e96055a95c8ceda
ztyibvrtoh-blip <ztyibvrtoh@gmail.com>
2026-08-02 21:35:48 +0100

    Update server.js

--- server.js
+++ server.js
@@ -2,12 +2,12 @@ const express = require("express");
 
 const app = express();
 
-const PORT = process.env.PORT || 3000;
-
 app.get("/", (req, res) => {
-  res.send("DRAGONNOIR Backend is working!");
+  res.send("Hello Railway");
 });
 
-app.listen(PORT, () => {
-  console.log(`Server started on port ${PORT}`);
+const PORT = process.env.PORT || 3000;
+
+app.listen(PORT, "0.0.0.0", () => {
+  console.log("Server running on port " + PORT);
 });
